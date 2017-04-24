9f49ea54cc86c8a01af6d7d19d9cf9a1de617339
Chinaski <emindanew@yandex.ru>
2017-04-25 00:25:50 +0800

    Добавил библиотеку vis.h

---
+++ vis.h
@@ -0,0 +1,15 @@
+#ifndef VIS_H
+#define VIS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <Windows.h>
+#include <conio.h>
+#include <ctype.h>
+
+
+void ris(int step);
+
+
+#endif
