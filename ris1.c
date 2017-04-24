089c3a63360f6b6f6ecedb92c6b07f3b8526534f
Chinaski <emindanew@yandex.ru>
2017-04-24 21:11:39 +0800

    Добавил рисунки

--- ris1.c
+++ ris1.c
@@ -3,5 +3,196 @@
 
 void ris(int step)
 {
-	
+    if (step == 0)
+	{
+
+		printf("\n\t**********************");
+		printf("\n\t                      ");
+		printf("\n\t                      ");
+		printf("\n\t                      ");
+		printf("\n\t                      ");
+		printf("\n\t                      ");
+		printf("\n\t                      ");
+		printf("\n\t                      ");
+		printf("\n\t                      ");
+		printf("\n\t                      ");
+		printf("\n\t                      ");
+		printf("\n\t                      ");
+		printf("\n\t                      ");
+		printf("\n\t                      ");
+		printf("\n\t                      ");
+		printf("\n\t**********************");
+	}
+	if (step == 1)
+	{
+
+		printf("\n\t**********************");
+		printf("\n\t                      ");
+		printf("\n\t                      ");
+		printf("\n\t                      ");
+		printf("\n\t                      ");
+		printf("\n\t                      ");
+		printf("\n\t                      ");
+		printf("\n\t                      ");
+		printf("\n\t                      ");
+		printf("\n\t                      ");
+		printf("\n\t                      ");
+		printf("\n\t                      ");
+		printf("\n\t  -----------------   ");
+		printf("\n\t  |               |   ");
+		printf("\n\t                      ");
+		printf("\n\t**********************");
+	}
+	if (step == 2)
+	{
+		printf("\n\t**********************");
+		printf("\n\t                      ");
+		printf("\n\t                      ");
+		printf("\n\t  |                   ");
+		printf("\n\t  |                   ");
+		printf("\n\t  |                   ");
+		printf("\n\t  |                   ");
+		printf("\n\t  |                   ");
+		printf("\n\t  |                   ");
+		printf("\n\t  |                   ");
+		printf("\n\t  |                   ");
+		printf("\n\t  |                   ");
+		printf("\n\t  -----------------   ");
+		printf("\n\t  |               |   ");
+		printf("\n\t                      ");
+		printf("\n\t**********************");
+	}
+	if (step == 3)
+	{
+		printf("\n\t**********************");
+		printf("\n\t                      ");
+		printf("\n\t  -----------         ");
+		printf("\n\t  |                   ");
+		printf("\n\t  |                   ");
+		printf("\n\t  |                   ");
+		printf("\n\t  |                   ");
+		printf("\n\t  |                   ");
+		printf("\n\t  |                   ");
+		printf("\n\t  |                   ");
+		printf("\n\t  |                   ");
+		printf("\n\t  |                   ");
+		printf("\n\t  -----------------   ");
+		printf("\n\t  |               |   ");
+		printf("\n\t                      ");
+		printf("\n\t**********************");
+	}
+	if (step == 4)
+	{
+		printf("\n\t**********************");
+		printf("\n\t                      ");
+		printf("\n\t  -----------         ");
+		printf("\n\t  |         |         ");
+		printf("\n\t  |         |         ");
+		printf("\n\t  |         -         ");
+		printf("\n\t  |                   ");
+		printf("\n\t  |                   ");
+		printf("\n\t  |                   ");
+		printf("\n\t  |                   ");
+		printf("\n\t  |       _____       ");
+		printf("\n\t  |       |   |       ");
+		printf("\n\t  -----------------   ");
+		printf("\n\t  |               |   ");
+		printf("\n\t                      ");
+		printf("\n\t**********************");
+	}
+	if (step == 5)
+	{
+		printf("\n\t**********************");
+		printf("\n\t                      ");
+		printf("\n\t  -----------         ");
+		printf("\n\t  |         |         ");
+		printf("\n\t  |         |         ");
+		printf("\n\t  |         -         ");
+		printf("\n\t  |        (o)        ");
+		printf("\n\t  |                   ");
+		printf("\n\t  |                   ");
+		printf("\n\t  |                   ");
+		printf("\n\t  |       _____       ");
+		printf("\n\t  |       |   |       ");
+		printf("\n\t  -----------------   ");
+		printf("\n\t  |               |   ");
+		printf("\n\t                      ");
+		printf("\n\t**********************");
+	}
+	if (step == 6)
+	{
+		printf("\n\t**********************");
+		printf("\n\t                      ");
+		printf("\n\t  -----------         ");
+		printf("\n\t  |         |         ");
+		printf("\n\t  |         |         ");
+		printf("\n\t  |         -         ");
+		printf("\n\t  |        (o)        ");
+		printf("\n\t  |         0         ");
+		printf("\n\t  |                   ");
+		printf("\n\t  |                   ");
+		printf("\n\t  |       _____       ");
+		printf("\n\t  |       |   |       ");
+		printf("\n\t  -----------------   ");
+		printf("\n\t  |               |   ");
+		printf("\n\t                      ");
+		printf("\n\t**********************");
+	}
+	if (step == 7)
+	{
+		printf("\n\t**********************");
+		printf("\n\t                      ");
+		printf("\n\t  -----------         ");
+		printf("\n\t  |         |         ");
+		printf("\n\t  |         |         ");
+		printf("\n\t  |         -         ");
+		printf("\n\t  |        (o)        ");
+		printf("\n\t  |        /0\\       ");
+		printf("\n\t  |                   ");
+		printf("\n\t  |                   ");
+		printf("\n\t  |       _____       ");
+		printf("\n\t  |       |   |       ");
+		printf("\n\t  -----------------   ");
+		printf("\n\t  |               |   ");
+		printf("\n\t                      ");
+		printf("\n\t**********************");
+	}
+	if (step == 8)
+	{
+		printf("\n\t**********************");
+		printf("\n\t                      ");
+		printf("\n\t  -----------         ");
+		printf("\n\t  |         |         ");
+		printf("\n\t  |         |         ");
+		printf("\n\t  |         -         ");
+		printf("\n\t  |        (o)        ");
+		printf("\n\t  |        /0\\       ");
+		printf("\n\t  |        / \\       ");
+		printf("\n\t  |                   ");
+		printf("\n\t  |       _____       ");
+		printf("\n\t  |       |   |       ");
+		printf("\n\t  -----------------   ");
+		printf("\n\t  |               |   ");
+		printf("\n\t                      ");
+		printf("\n\t**********************");
+	}
+	if (step == 9)
+	{
+		printf("\n\t**********************");
+		printf("\n\t                      ");
+		printf("\n\t  -----------         ");
+		printf("\n\t  |         |         ");
+		printf("\n\t  |         |         ");
+		printf("\n\t  |         -         ");
+		printf("\n\t  |        (X)        ");
+		printf("\n\t  |        /0\\       ");
+		printf("\n\t  |        / \\       ");
+		printf("\n\t  |                   ");
+		printf("\n\t  |       _____       ");
+		printf("\n\t  |       |   |       ");
+		printf("\n\t  -----------------   ");
+		printf("\n\t  |               |   ");
+		printf("\n\t                      ");
+		printf("\n\t**********************");
+	}	
 }
