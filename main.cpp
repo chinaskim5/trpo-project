c241db1ceb8e1ce635faa2b168a71780e173e3d2
Chinaski <emindanew@yandex.ru>
2017-04-30 20:02:28 +0800

    Добавил в си ++

---
+++ main.cpp
@@ -0,0 +1,297 @@
+#include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <Windows.h>
+#include <conio.h>
+#include <ctype.h>
+#include <iostream>
+
+
+void ris(int step)
+{
+	switch (step)
+	{
+	case 0:
+		{
+			printf("\n\t**********************");
+			printf("\n\t                      ");
+			printf("\n\t                      ");
+			printf("\n\t                      ");
+			printf("\n\t                      ");
+			printf("\n\t                      ");
+			printf("\n\t                      ");
+			printf("\n\t                      ");
+			printf("\n\t                      ");
+			printf("\n\t                      ");
+			printf("\n\t                      ");
+			printf("\n\t                      ");
+			printf("\n\t                      ");
+			printf("\n\t                      ");
+			printf("\n\t                      ");
+			printf("\n\t**********************");
+			break;
+		}
+	case 1:
+		{
+
+			printf("\n\t**********************");
+			printf("\n\t                      ");
+			printf("\n\t                      ");
+			printf("\n\t                      ");
+			printf("\n\t                      ");
+			printf("\n\t                      ");
+			printf("\n\t                      ");
+			printf("\n\t                      ");
+			printf("\n\t                      ");
+			printf("\n\t                      ");
+			printf("\n\t                      ");
+			printf("\n\t                      ");
+			printf("\n\t  -----------------   ");
+			printf("\n\t  |               |   ");
+			printf("\n\t                      ");
+			printf("\n\t**********************");
+			break;
+		}
+	case 2:
+		{
+			printf("\n\t**********************");
+			printf("\n\t                      ");
+			printf("\n\t                      ");
+			printf("\n\t  |                   ");
+			printf("\n\t  |                   ");
+			printf("\n\t  |                   ");
+			printf("\n\t  |                   ");
+			printf("\n\t  |                   ");
+			printf("\n\t  |                   ");
+			printf("\n\t  |                   ");
+			printf("\n\t  |                   ");
+			printf("\n\t  |                   ");
+			printf("\n\t  -----------------   ");
+			printf("\n\t  |               |   ");
+			printf("\n\t                      ");
+			printf("\n\t**********************");
+			break;
+		}
+	case 3 :
+		{
+			printf("\n\t**********************");
+			printf("\n\t                      ");
+			printf("\n\t  -----------         ");
+			printf("\n\t  |                   ");
+			printf("\n\t  |                   ");
+			printf("\n\t  |                   ");
+			printf("\n\t  |                   ");
+			printf("\n\t  |                   ");
+			printf("\n\t  |                   ");
+			printf("\n\t  |                   ");
+			printf("\n\t  |                   ");
+			printf("\n\t  |                   ");
+			printf("\n\t  -----------------   ");
+			printf("\n\t  |               |   ");
+			printf("\n\t                      ");
+			printf("\n\t**********************");
+			break;
+		}
+	case 4 :
+		{
+			printf("\n\t**********************");
+			printf("\n\t                      ");
+			printf("\n\t  -----------         ");
+			printf("\n\t  |         |         ");
+			printf("\n\t  |         |         ");
+			printf("\n\t  |         -         ");
+			printf("\n\t  |                   ");
+			printf("\n\t  |                   ");
+			printf("\n\t  |                   ");
+			printf("\n\t  |                   ");
+			printf("\n\t  |       _____       ");
+			printf("\n\t  |       |   |       ");
+			printf("\n\t  -----------------   ");
+			printf("\n\t  |               |   ");
+			printf("\n\t                      ");
+			printf("\n\t**********************");
+			break;
+		}
+	case 5 :
+		{
+			printf("\n\t**********************");
+			printf("\n\t                      ");
+			printf("\n\t  -----------         ");
+			printf("\n\t  |         |         ");
+			printf("\n\t  |         |         ");
+			printf("\n\t  |         -         ");
+			printf("\n\t  |        (o)        ");
+			printf("\n\t  |                   ");
+			printf("\n\t  |                   ");
+			printf("\n\t  |                   ");
+			printf("\n\t  |       _____       ");
+			printf("\n\t  |       |   |       ");
+			printf("\n\t  -----------------   ");
+			printf("\n\t  |               |   ");
+			printf("\n\t                      ");
+			printf("\n\t**********************");
+			break;
+		}
+	case 6 :
+		{
+			printf("\n\t**********************");
+			printf("\n\t                      ");
+			printf("\n\t  -----------         ");
+			printf("\n\t  |         |         ");
+			printf("\n\t  |         |         ");
+			printf("\n\t  |         -         ");
+			printf("\n\t  |        (o)        ");
+			printf("\n\t  |         0         ");
+			printf("\n\t  |                   ");
+			printf("\n\t  |                   ");
+			printf("\n\t  |       _____       ");
+			printf("\n\t  |       |   |       ");
+			printf("\n\t  -----------------   ");
+			printf("\n\t  |               |   ");
+			printf("\n\t                      ");
+			printf("\n\t**********************");
+			break;
+		}
+	case 7:
+		{
+			printf("\n\t**********************");
+			printf("\n\t                      ");
+			printf("\n\t  -----------         ");
+			printf("\n\t  |         |         ");
+			printf("\n\t  |         |         ");
+			printf("\n\t  |         -         ");
+			printf("\n\t  |        (o)        ");
+			printf("\n\t  |        /0\\       ");
+			printf("\n\t  |                   ");
+			printf("\n\t  |                   ");
+			printf("\n\t  |       _____       ");
+			printf("\n\t  |       |   |       ");
+			printf("\n\t  -----------------   ");
+			printf("\n\t  |               |   ");
+			printf("\n\t                      ");
+			printf("\n\t**********************");
+			break;
+		}
+	case 8 :
+		{
+			printf("\n\t**********************");
+			printf("\n\t                      ");
+			printf("\n\t  -----------         ");
+			printf("\n\t  |         |         ");
+			printf("\n\t  |         |         ");
+			printf("\n\t  |         -         ");
+			printf("\n\t  |        (o)        ");
+			printf("\n\t  |        /0\\       ");
+			printf("\n\t  |        / \\       ");
+			printf("\n\t  |                   ");
+			printf("\n\t  |       _____       ");
+			printf("\n\t  |       |   |       ");
+			printf("\n\t  -----------------   ");
+			printf("\n\t  |               |   ");
+			printf("\n\t                      ");
+			printf("\n\t**********************");
+			break;
+		}
+	case 9:
+		{
+			printf("\n\t**********************");
+			printf("\n\t                      ");
+			printf("\n\t  -----------         ");
+			printf("\n\t  |         |         ");
+			printf("\n\t  |         |         ");
+			printf("\n\t  |         -         ");
+			printf("\n\t  |        (X)        ");
+			printf("\n\t  |        /0\\       ");
+			printf("\n\t  |        / \\       ");
+			printf("\n\t  |                   ");
+			printf("\n\t  |       _____       ");
+			printf("\n\t  |       |   |       ");
+			printf("\n\t  -----------------   ");
+			printf("\n\t  |               |   ");
+			printf("\n\t                      ");
+			printf("\n\t**********************");
+			break;
+		}
+	}
+}
+
+
+
+int main()
+{
+	
+	setlocale(LC_CTYPE, "Russian");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	const int maxLetters = 10;
+	const int numberStep = 10;
+
+
+	char letters[] = "человек";
+	char inputChar = 0;
+	char outchar[] = { '_', '_', '_', '_', '_', '_', '_', '_' };
+
+
+	int pravilno=0;
+	int step = 0;
+	int errors = 1;
+	int number = 0;
+//	ris(step);
+	//printf("\n\n\t\t");
+	//for (int i = 0; i < strlen(letters); i++)
+	//{
+	//	printf("_ ");
+	//}
+	do
+	{
+		pravilno = 0;
+		if (errors == strlen(letters))
+		{
+			step++;
+		}
+		system("cls");
+		ris(step);
+		printf("\n\n\t\t");
+
+		for (int i = 0; i < strlen(letters); i++)
+		{
+			if (inputChar == letters[i])
+			{
+				outchar[i] = letters[i];
+			}
+		}
+		for (int i = 0; i < strlen(letters); i++)
+		{
+			if (outchar[i] == letters[i])
+			{
+				pravilno++;
+			}
+		}
+		
+		for (int i = 0; i < strlen(letters); i++)
+		{
+			printf("%c ", outchar[i]);
+		}
+		inputChar=_getch();
+		errors = 0;
+		for (int i = 0; i < strlen(letters); i++)
+		{
+			if (inputChar != letters[i])
+			{
+				errors++;
+			}
+		}
+	} 
+	while ((step < numberStep-1) && (pravilno < strlen(letters)));
+	if (pravilno == 7)
+	{
+		printf("\n\tПоздравляю вы отгадали слово\n");
+	}
+	else
+	{
+		printf("\nВы не отгадали слово\n");
+	}
+	return 0;
+}
